5e430d5039f7e82aa297fe458ac8a95652f01016
kshapaksha <ppkshapaksha@gmail.com>
2025-09-30 20:57:20 +0530

    Create area_triangle.c

---
+++ 1stSemester/Basics/area_triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    float p, q, r, sp, area;
+    printf("Enter the sides of a triangle:\t ");
+    scanf("%f %f %f", &p, &q, &r);
+    sp = (p + q + r) / 2; 
+    area = sqrt(sp * (sp - p) * (sp - q) * (sp - r)); 
+    printf("Calculated area of triangle is %6.2f square units\n", area);
+    return 0;
+}
