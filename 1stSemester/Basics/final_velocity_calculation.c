8cda7d016f7d7638dbd0d12c7537a0eb64927e20
kshapaksha <ppkshapaksha@gmail.com>
2025-09-30 20:53:20 +0530

    Create final_velocity_calculation.c

---
+++ 1stSemester/Basics/final_velocity_calculation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    float u,v,acc,time;
+    printf("Enter the value of initial velocity in m/s:\t");
+    scanf("%f",&u);
+    printf("Enter the amount of acceleration:\t");
+    scanf("%f",&acc);
+    printf("Enter the time in sec:\t");
+    scanf("%f",&time);
+    v=u + acc * time; 
+    printf("Velocity after %6.2f sec is %6.2f m/s\n", time, v);
+    return 0;
+}
