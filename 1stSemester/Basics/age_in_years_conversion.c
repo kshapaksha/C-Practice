2065735838656dc6aabbd652715706b5b61d42cc
kshapaksha <ppkshapaksha@gmail.com>
2025-09-30 20:55:28 +0530

    Create age_in_years_conversion.c

---
+++ 1stSemester/Basics/age_in_years_conversion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    int age;
+    float ageInSec;
+    printf("What is your age (years)?\t ");
+    scanf("%d", &age);
+    ageInSec = 3.156E7 * age; 
+    printf("Your age in seconds is %5.2E", ageInSec);
+    return 0;
+}
